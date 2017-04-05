2419bcaea1210253faf3f3c0210a18e62fb866ba
taphan <trucphan307@gmail.com>
2017-04-05 11:41:21 +0200

    Lag nødvendige metoder for navigering gjennom appen (ved å klikke på symbolene)

--- src/globalWalk/view/MasterController.java
+++ src/globalWalk/view/MasterController.java
@@ -1,4 +1,35 @@
 package globalWalk.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+
 public class MasterController {
+
+    // Profil siden bruker denne som Controller, siden den har ingen spesielle funksjoner
+
+    public void clickProfile(MouseEvent mouseEvent) {
+        System.out.println("Går til profil siden");
+    }
+
+    public void clickList(MouseEvent mouseEvent) {
+        System.out.println("Går til list siden");
+    }
+
+    public void clickCompetition(MouseEvent mouseEvent) {
+        System.out.println("Går til konkurranse siden");
+    }
+
+    public void clickHome(MouseEvent mouseEvent) {
+        System.out.println("Går til hjemmesiden");
+    }
+
+    public void clickStep(MouseEvent mouseEvent) {
+        System.out.println("Går til pedometer siden");
+    }
+
+    public void clickSettings(MouseEvent mouseEvent) {
+        System.out.println("Går til innstilinger");
+    }
 }
