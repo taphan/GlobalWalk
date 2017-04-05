d60d3571f9f707f7190159ca7651c5f3be1242a3
Siren Johansen <sirenjoh@hotmail.com>
2017-04-05 10:35:39 +0200

    Map structure changed + add pedometer

---
+++ src/globalWalk/view/PedometerController.java
@@ -0,0 +1,20 @@
+package globalWalk.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.text.Text;
+
+public class PedometerController {
+	
+	@FXML private TextField addSteps;
+	@FXML private Text dailySteps;
+	@FXML private Text leftSteps;
+	@FXML private Button addStepsbutton;
+	@FXML private ToggleButton manual;
+	
+	
+	
+
+}
